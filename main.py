33bc0465b85c58a6f9c851c4df55cca761609652
Adam Dzacovsky <adzjr09@gmail.com>
2025-08-19 20:15:20 +0200

    doplnenie predosleho commitu

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ class Chatbot:
     def __init__(self, name = "Chatbotv1"):
         self.name = name
     
-    def pozdrav(self):
+    def greet(self):
         return f"Ahoj, moje meno je {self.name}"
     
+    def respond(self,user_input):
+        user_input = user_input.lower()
+
+        if "ahoj" in user_input:
+            return "ahoj vitaj v chate"
+        elif "koniec" in user_input:
+            return "maj sa pekne"
+        else:
+            return "toto zataial nerozumiem"
+    
     def chat(self):
-        print(self.pozdrav())
+        print(self.greet())
         
 
 
