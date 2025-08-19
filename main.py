2eb06a502f4224c2487a52fe08fa9dbfda58da4f
Adam Dzacovsky <adzjr09@gmail.com>
2025-08-19 19:57:47 +0200

    vytvorenie objektu s nazvom bot

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 class Chatbot: 
     def __init__(self, name = "Chatbotv1"):
         self.name = name
+    
+    def pozdrav(self):
+        print("Zavolala sa funkcia pozdrav " + self.name)
+        return f"Ahoj, moje meno je {self.name}"
         
+
+
+
+bot = Chatbot("Bot1")
+
+
+
+
